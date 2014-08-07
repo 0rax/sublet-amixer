eff3e69452a2dafd70a3fa88404f5801717845bf
0rax <roemer.jp@gmail.com>
2014-08-07 21:40:40 +0200

    Add v0.1 of amixer sublet

---
+++ amixer.rb
@@ -0,0 +1,88 @@
+# Amixer sublet file
+# Created with sur-0.1
+configure :amixer do |s| # {{{
+  s.interval = s.config[:interval]      || 5
+  s.device   = s.config[:device]        || "pulse"
+  s.prefix   = s.config[:prefix]        || " Vol: "
+  s.suffix   = s.config[:suffix]        || " >"
+end # }}}
+
+helper do
+  def get_dev(device="pulse", chan="Master")
+    `amixer -D "#{device}" get "#{chan}"`
+  end
+
+  def toggle_mute(device="pulse", chan="Master")
+    `amixer -D "#{device}" set "#{chan}" toggle`
+  end
+
+  def vol_minus(step=5, device="pulse", chan="Master")
+    `amixer -D "#{device}" set "#{chan}" playback "#{step}%-"`
+  end
+
+  def vol_plus(step=5, device="pulse", chan="Master")
+    `amixer -D "#{device}" set "#{chan}" playback "#{step}%+"`
+  end
+
+  def set_volume(vol)
+    if vol
+      vstr = "%3i%" % vol.to_s
+    else
+      vstr = "mute"
+    end
+
+    self.data = self.prefix + vstr + self.suffix
+  end
+
+  def get_volume(devInfo)
+    vol = devInfo.match(/\[([0-9]+)%\] \[(on|off)\]/)
+    volume = vol.captures[0].to_i
+    if vol.captures[1] == "on"
+      volume
+    else
+      nil
+    end
+  end
+end
+
+on :run do |s| # {{{
+  dev = get_dev(s.device, "Master")
+  vol = get_volume(dev)
+
+  set_volume(vol)
+end # }}}
+
+on :mouse_down do |s, x, y, button| # {{{
+  if button == 3
+    dev = toggle_mute(s.device)
+  elsif button == 4
+    dev = vol_plus(1, s.device)
+  elsif button == 5
+    dev = vol_minus(1, s.device)
+  end
+  vol = get_volume(dev)
+
+  set_volume(vol)
+end # }}}
+
+# Grabs
+grab :AudioRaise do |s| # {{{
+  dev = vol_plus(s.step, s.device)
+  vol = get_volume(dev)
+
+  set_volume(vol)
+end # }}}
+
+grab :AudioLower do |s| # {{{
+  dev = vol_minus(s.step, s.device)
+  vol = get_volume(dev)
+
+  set_volume(vol)
+end # }}}
+
+grab :AudioMute do |s| # {{{
+  dev = toggle_mute(s.device)
+  vol = get_volume(dev)
+
+  set_volume(vol)
+end # }}}
---
+++ amixer.spec
@@ -0,0 +1,53 @@
+# AMixer specification file
+# Created with sur-0.2.143
+Sur::Specification.new do |s|
+  s.name        = "AMixer"
+  s.authors     = [ "0rax" ]
+  s.contact     = "roemer.jp@gmail.com"
+  s.date        = "Tue Aug  5 11:44:23 2014"
+  s.description = "Display and control the volume of an ALSA device"
+  s.notes       = <<NOTES
+This sublet brings you AlsaMixer to subtle.
+It shows the volume of a specific ALSA device.
+If the device is availlable in AlsaMixer/Amixer, you can control it with this sublet.
+You only need to have alsa-utils installed.
+
+Bindings:
+Left click toggles mute, mouse wheel up and down changes the volume.
+NOTES
+  s.config = [
+    {
+	:name        => "step",
+	:type        => "integer",
+	:description => "Volume increase/decrease steps",
+	:def_value   => "5"
+    },
+    {
+	:name        => "device",
+	:type        => "string",
+	:description => "Device to get/set volume from/to",
+	:def_value   => "pulse"
+    },
+    {
+	:name        => "prefix",
+	:type        => "string",
+	:description => "Prefix",
+	:def_value   => " Vol: "
+    },
+    {
+	:name        => "suffix",
+	:type        => "string",
+	:description => "Suffix",
+	:def_value   => " >"
+    }
+  ]
+  s.version     = "0.1"
+  s.tags        = [ "Amixer", "Volume", "Linux", "Config", "Grabs", "AlsaMixer", "MouseWheel" ]
+  s.files       = [ "amixer.rb" ]
+
+  s.grabs = {
+      :AudioRaise  => "Increase volume",
+      :AudioLower  => "Decrease volume",
+      :AudioMute   => "Toggle mute"
+  }
+end
